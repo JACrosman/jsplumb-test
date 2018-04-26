0b1c75093311f82a85d1d331c9e827318c9eb89a
Joshua Crosman <jacrosman@gmail.com>
2018-04-26 18:59:58 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const jsPlumb = window.jsPlumb;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    jsPlumb.ready(() => {
+      jsPlumb.connect({
+        source: 'window1',
+        target: 'window2',
+        paintStyle: { strokeWidth: 2, stroke: 'rgb(243,230,18)' },
+        endpointStyle: { fill: 'rgb(243,229,`0)' },
+        anchors: ['Right', 'Right'],
+        connector: ['Flowchart', {
+          cornerRadius: 5
+        }],
+        overlays: [
+          'Arrow',
+          ['Label', { label: 'foo', location: 0.25 }]
+        ],
+      });
+    });
+  }
 }
